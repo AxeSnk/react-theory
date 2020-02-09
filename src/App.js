9ae435b2fbf5fbd8a41eb5d731013d8fc548f564
Alexandr Axenov <alexanderaxenov@mail.ru>
2020-02-09 16:35:32 +0800

    learn 12

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Car from './Car/Car'
 
 function App() {
+  const divStyle = {
+    textAlign: "center"
+  };
+
   return (
-    <div>
-      <div className="App">
-        <h1>Hello word!</h1>
-      </div>
-      <p>Hello</p>
+    <div style={divStyle}>
+      <h1 style={{ color: "blue", fontSize: "20px" }}>Hello word!</h1>
+      <Car name={'Ford'} year={2018}>
+        <p style={{color: 'blue'}}>COLOR</p>
+      </Car>
+      <Car name='Audi' year={2016}>
+        <p style={{color: 'red'}}>COLOR</p>
+      </Car>
+      <Car name={'Mazda'} year={2020} />
+
     </div>
   );
 }
